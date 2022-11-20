b90b6ef742bcd834fb91131b1835d670e5776094
akshatajinagond91 <www.akshatajinagond91@gmail.com>
2022-11-20 18:37:51 +0530

    java collections

---
+++ Day3/src/StudentInterface.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public interface StudentInterface {
+	
+	public void addStudent(Student student);
+    public void deleteStudents(int id);
+    public void findStudent(int id);
+    public List<Student> findStudentWithPincode(String pin);
+    public void sortWithName();
+    public void setOfMarks();
+    public void addAllStudents(List<Student> stud);
+}
