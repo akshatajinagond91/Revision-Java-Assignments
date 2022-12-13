9188a348094d8ba6efa2b2308b2af76862185d78
akshatajinagond91 <www.akshatajinagond91@gmail.com>
2022-12-13 16:41:12 +0530

    calendar api

---
+++ MsaiMailApplication/src/main/java/com/masai/MsaiMailApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaiMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaiMailApplication.class, args);
+	}
+
+}
---
+++ demo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Akshu
+ *
+ */
+module demo {
+}
